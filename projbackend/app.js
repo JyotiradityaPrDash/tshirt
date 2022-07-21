06717db57b654fc09b98a7b2cc2318305db9bf03
Jyotiraditya Prasad Dash <dashjyotiraditya@gmail.com>
2022-07-22 00:50:50 +0530

    Mongo Database Connection in app.js

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,16 +1,18 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
 //mongoose.connect('mongodb://localhost:27017/tshirt', 
 //{useNewUrlParser: true});
-mongoose.connect("mongodb://localhost:27017/test", {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 }).then(() => {
     console.log("DB CONNECTED")
-})
+});
 
 
 const port = process.env.PORT || 8000;
