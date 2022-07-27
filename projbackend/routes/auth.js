2ce263766ef492994225d92e0d8bf4fd95c81029
Jyotiraditya Prasad Dash <dashjyotiraditya@gmail.com>
2022-07-27 20:21:10 +0530

    Signin Route Added

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -16,7 +16,7 @@ router.post("/signin", [
     check("email", "email is required").isEmail(),
     check("password", "password field is required").isLength({min: 3})
 
-], signup);
+], signin);
 
 router.get("/signout", signout)
 
