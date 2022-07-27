4496bf770ec9762c4b52a6aeb5a08cf2341179b7
Jyotiraditya Prasad Dash <dashjyotiraditya@gmail.com>
2022-07-27 22:19:24 +0530

    Signin Route Authenticated

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { check, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup = (req,res) =>{
 
@@ -23,6 +25,38 @@ exports.signup = (req,res) =>{
             email: user.email,
             id:user._id
         });
+    })
+};
+
+exports.signin = (req, res) => {
+    const errors = validationResult(req)
+    const {email, password} = req.body;
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            errors: errors.array()[0].msg
+        });
+    }
+
+    User.findOne({email}, (err, user) => {
+        if(err){
+            res.status(400).json({
+                error: "USER email does not exist"
+            });
+        }
+        if(!user.autheticate(password)){
+             return res.status(401).json({
+                error: "Email and password do not match"
+            });
+        }
+        
+        const token = jwt.sign({_id: user._id}, process.env.SECRET)
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999})
+
+        //send response to front end
+        const {_id, name, email, role} = user;
+        return res.json({token, user:{_id, name, email, role}})
     })
 };
 
